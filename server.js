dcf288348562873a188326fe07124140c8f8f7eb
Arnab Pachal <ap.23cs8031@nitdgp.ac.in>
2024-12-05 16:45:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const cors = require('cors');
 const app = express();
 // Middleware
 app.use(cors({
-    origin: process.env.FRONTEND_URL, 
+    origin:"*", 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }));
