3455ad9a668d766535c7709b181bb13e67b6026a
Arnab Pachal <ap.23cs8031@nitdgp.ac.in>
2024-12-15 15:52:51 +0530

    commited

--- vidschema.js
+++ vidschema.js
@@ -1,10 +1,9 @@
 const mongoose = require("mongoose");
 require('dotenv').config();
-mongoose.connect(`mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.ustzk.mongodb.net/myDatabase?retryWrites=true&w=majority`)
+mongoose.connect(`mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@cluster0.ustzk.mongodb.net/mypass?retryWrites=true&w=majority`)
   .then(() => console.log('Connected to MongoDB Atlas!'))
   .catch((error) => console.error('Error connecting to MongoDB Atlas:', error));
 const schema = new mongoose.Schema({
-public_id : String,
 name : String,    
 url : String,
 })    
